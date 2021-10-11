f03cb31ff1c0bc083e93882de5c23e427929acc1
Vinicius Souza <viisouza10@live.com>
2021-10-10 22:04:54 -0300

    refactor: delete duplicate components

--- src/components/SubTitle.tsx
+++
@@ -1,9 +0,0 @@
-interface SubTitleProps {
-    text:string;
-    color?:"primary" |"secondary";
-    className?:string;
-}
-
-export const SubTitle = ({text,color = "primary",className}:SubTitleProps) =>{
-    return <h2 className={`text-${color}-400 uppercase font-bold text-sm sm:text-2xl ${className}`}>{text}</h2>
-}
